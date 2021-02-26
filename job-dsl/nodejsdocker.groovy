ad032da11732ec6180b9bcd11fcbd6d9d63d3213
nirajKudos <75608961+nirajKudos@users.noreply.github.com>
2021-02-26 11:00:57 +0530

    Update nodejsdocker.groovy
    credential change

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/nirajKudos/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('nirajKudos')
+            node / gitConfigEmail('niraj@kudosfinance.in')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('nirajKudos/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
