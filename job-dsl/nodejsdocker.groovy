028d9389b9a799a2c2517a71fe0862a313a6cec5
nirajKudos <75608961+nirajKudos@users.noreply.github.com>
2021-02-26 13:51:51 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('Nodejs Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('nirajKudos/dockernodejsdemo')
+            repositoryName('nirajKudos/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
